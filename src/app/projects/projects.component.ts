4083f5ec54e22672c48763b939828269bbb09d7e
andynogamer <antoniomz769@gmail.com>
2025-06-11 13:18:29 -0600

    first commit

---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbModule, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [CommonModule, NgbModule],
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css']
+})
+export class ProjectsComponent {
+
+  
+}
